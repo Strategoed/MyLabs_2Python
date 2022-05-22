7f3f2beec8f7fe66141ccd24044dc54266274a94
Strategoed <strategoed@gmail.com>
2022-05-22 16:49:53 +0300

    Добавил первую лабораторную работу

---
+++ Lab1/Library.py
@@ -0,0 +1,37 @@
+def CreateSFile(path, path2):
+    file = open(path, "r")
+    old_lines = file.read().splitlines()
+    file.close()
+    new_file = open(path2, "w")
+    for i in range(len(old_lines)):
+        old_lines[i] = str(len(old_lines[i])) + ": " + old_lines[i]
+    new_file.write('\n'.join(old_lines))
+    new_file.close()
+
+def OutputFile(path):
+    file = open(path, "r")
+    file_cont = file.read()
+    file.close()
+    print("\n" + path + ":\n")
+    print(file_cont)
+
+def CreateFFile(path):
+    print("\nInput lines\nPress <ENTER> for end line\nPress ';' for end file input\n")
+    lines = []
+    while True:
+        line = input()
+        if line.find(";") != -1:
+            lines.append(line[0:-2])
+            break
+        else:
+            lines.append(line)
+    file = open(path, "w")
+    file.write('\n'.join(lines))
+    file.close()
+
+def InputFilePath():
+    path = input("Input file path: ")
+    if (path.endswith(".txt")):
+        return path
+    else:
+        return path + ".txt"
---
+++ Lab1/main.py
@@ -0,0 +1,8 @@
+from Library import *
+
+Path1 = InputFilePath()
+Path2 = InputFilePath()
+CreateFFile(Path1)
+OutputFile(Path1)
+CreateSFile(Path1, Path2)
+OutputFile(Path2)
